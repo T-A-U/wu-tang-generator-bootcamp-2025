02a5dd77fa62d2f5e8cde35fd86177a1f43e7d0e
T-A-U <153134801+T-A-U@users.noreply.github.com>
2025-10-17 02:07:16 -0400

    Functionality, extended goals looked into

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
 //made with help
-function generateWuTang() {
-    const q = document.forms.wuQuestionaire;
-    const parts = [q.q1.value, q.q3.value, q.q4.value];
-    const name = parts.join('');
-    document.getElementById('result').textContent = `Your generated name: ${name}`;
-}
-
-const wuBtn = document.getElementById('wuBtn');
-    wuBtn.addEventListener('click', () => {
-      generateWuTang();
+form = document.getElementById('wuQuestionaire') 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    // generateWuTang();
 })
+
+function generateWuTang() {
+    // const q = document.forms.wuQuestionaire;
+    // console.log(q)
+    // const parts = [form.elements.q1.value, form.elements.q2.value, form.elements.q5.value];
+    // console.log(parts)
+    // const name = parts.join('');
+    // console.log(name)
+   
+    // const formData = new FormData(form) 
+    // console.log(form.elements.q3.value)
+    let name = ''
+    if(form.elements.q2.value >3 ){
+        name += "The coolest "
+    }
+    if(form.elements.q2.value <3 ){
+        name += "The geeky "
+    }
+    if(form.elements.q2.value >3 ){
+        name += "The average "
+    }
+    if(form.elements.q3.value === 'Wealth'){
+        name+= "MoneyBagz "
+    }
+    if(form.elements.q3.value === 'Knowledge'){
+        name+= "Scholarly Shadow "
+    }
+    if(form.elements.q3.value === 'Power'){
+        name+= "Stregth driven "
+    }
+    if(form.elements.q3.value === 'Patience'){
+        name+= "Timeless "
+    }
+    if(form.elements.q4.value === 'Flow'){
+        name+= "Lyricle "
+    }
+    if(form.elements.q4.value === 'Cadence'){
+        name+= "Miracle "
+    }
+    if(form.elements.q4.value === 'Difficulty'){
+        name+= "Masterful "
+    }
+    if(form.elements.q4.value === 'Yes'){
+        name+= "Magus "
+    }
+    if(form.elements.q4.value === 'No'){
+        name+= "Masterful "
+    }
+
+    //refactor into more wutang sounding 
+
+    document.getElementById('result').textContent = `Your generated name: ${name}`;
+}  
+
+
+
+//some wutang names to add to an array which  we add
+//Jade General
+// Fresh Protector
+// Bold Source
